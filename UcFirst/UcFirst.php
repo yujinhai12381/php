8052d0271635095fe8fb44df5d54887df9f04d7e
yujinhai <yujinhai12381@126.com>
2015-07-06 14:25:21 +0800

    for study ucfirst()

---
+++ UcFirst/UcFirst.php
@@ -0,0 +1,11 @@
+<?php
+#########################################################################
+# File Name: UcFirst.php
+# Desc: 
+# Author: yujinhai
+# mail: yujinhai12381@126.com
+# Created Time: Mon 06 Jul 2015 02:21:44 PM CST
+#########################################################################
+    error_log("only  study and test");
+    //error_log( ucfirst("hello world"));
+    echo ucfirst("hello world\n");
