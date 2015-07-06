5a47177cc07297fe5b0f8fdc02ae96875be5e1b6
yujinhai <yujinhai12381@126.com>
2015-07-06 14:02:34 +0800

    test number for true or false

---
+++ true.php
@@ -0,0 +1,15 @@
+<?php
+#########################################################################
+# File Name: test.php
+# Desc: 
+# Author: yujinhai
+# mail: yujinhai12381@126.com
+# Created Time: Mon 06 Jul 2015 11:58:24 AM CST
+#########################################################################
+
+$flag = 3;
+if (!$flag) {
+    error_log("flag ===  false");
+} else {
+    error_log("flag ===  true");
+}
