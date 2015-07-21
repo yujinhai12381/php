39d8075068fd3fa5d7bda8253feeb0b9338e22f0
yujinhai <yujinhai12381@126.com>
2015-07-21 17:44:32 +0800

    test 三目运算符

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+#########################################################################
+# File Name: test.php
+# Desc: 
+# Author: yujinhai
+# mail: yujinhai12381@126.com
+# Created Time: Mon 13 Jul 2015 07:05:17 PM CST
+#########################################################################
+
+if (!empty($var['1'])){
+    error_log("empty");
+} else {
+    error_log("not empty");
+}
