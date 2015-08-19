273e0db4e85ad48ea7b2e1ec564aeeac3c0fdc5d
yujinhai <yujinhai12381@126.com>
2015-08-19 18:43:40 +0800

    test bug

--- test.php
+++ test.php
@@ -6,8 +6,9 @@
 # mail: yujinhai12381@126.com
 # Created Time: Mon 13 Jul 2015 07:05:17 PM CST
 #########################################################################
-//$var['test'] = 0;
-if (empty($var['test'])) {
+$var['test'] = 0;
+$a = isset($var['test']) ? $var['test']: 0;
+if (empty($a)) {
     error_log("empty");
 } else {
     error_log("not empty");
