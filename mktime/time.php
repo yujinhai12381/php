0adde77f662127758375195e40579905c0a8561d
yujinhai <yujinhai@yongche.com>
2016-07-26 16:37:00 +0800

    test

---
+++ mktime/time.php
@@ -0,0 +1,10 @@
+<?php
+#########################################################################
+# File Name: mktime.php
+# Desc: 
+# Author: yujinhai
+# mail: yujinhai12381@126.com
+# Created Time: Fri 17 Jul 2015 06:54:10 PM CST
+#########################################################################
+$time = date("Y-m-d H:i:s");
+error_log("time:".$time);
---
+++ sprintf.php
@@ -0,0 +1,12 @@
+<?php
+#########################################################################
+# File Name: sprintf.php
+# Desc: 
+# Author:yujinhai
+# mail: yujinhai@yongche.com
+# Created Time: 二  7/12 15:25:45 2016
+#########################################################################
+
+$tmp = sprintf("%8x",time());
+error_log("result: ".($tmp));
+
