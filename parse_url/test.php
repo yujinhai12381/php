c8a2baa6b52aa0bb91a1dba525ad6404f42a7833
yujinhai <yujinhai@yongche.com>
2017-05-19 09:40:02 +0800

    test

---
+++ parse_url/test.php
@@ -0,0 +1,16 @@
+<?php
+#########################################################################
+# File Name: test.php
+# Desc: 
+# Author:yujinhai
+# mail: yujinhai@yongche.com
+# Created Time: 二  5/16 11:09:19 2017
+#########################################################################
+
+$url = 'http://yujinhai.xx.haha.org/V1/HomePage/driver?city=dandong';
+
+print_r(parse_url($url));
+
+echo parse_url($url, PHP_URL_PATH);
+echo "\n";
+
