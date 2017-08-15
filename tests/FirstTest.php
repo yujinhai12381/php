d7ee45f5bd93e0e76a86c629ceb7c6e2d2ac3268
yujinhai <yujinhai12381@126.com>
2017-08-15 18:20:20 +0800

    study composer

---
+++ tests/FirstTest.php
@@ -0,0 +1,20 @@
+<?php
+#########################################################################
+# File Name: FirstTest.php
+# Desc: 
+# Author:yujinhai
+# mail: yujinhai12381@126.com
+# Created Time: 二  8/15 17:40:24 2017
+#########################################################################
+#
+class FirstTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp(){ }
+        public function tearDown(){ }
+        public function testHello()
+        {
+            $ret = [];
+            $this->assertEmpty($ret);
+        }
+}
+
