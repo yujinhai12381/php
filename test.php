edc427c504842c9c69d400d9506a6d1e9fbfdc93
yujinhai <yujinhai12381@126.com>
2017-10-17 19:24:50 +0800

    line

--- test.php
+++ test.php
@@ -15,4 +15,5 @@ echo strtotime("+1 week 2 days 4 hours 2 seconds"), "\n";
 echo strtotime("next Thursday"), "\n";
 echo strtotime("last Monday"), "\n";
 error_log('test');
+error_log(__LINE__);
 ?>
