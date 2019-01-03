1b406b55f13c941dbb7756ac88504e66db396cf9
yujinhai <yujinhai12381@126.com>
2019-01-03 10:30:19 +0800

    update

---
+++ array/array_replace.php
@@ -0,0 +1,12 @@
+<?php
+#########################################################################
+# File Name: array_replace.php
+# Desc: 
+# Author:yujinhai
+# mail: yujinhai12381@126.com
+# Created Time: 2018年12月25日 星期二 20时54分51秒
+#########################################################################
+
+$a1=array("a"=>"red","b"=>"green");
+$a2=array("a"=>"orange","burgundy");
+print_r(array_replace($a1,$a2));
